a961a701fb5e391fc19d96cca24965c9b439f1fe
zhangyonghe <zhangyonghe.2009@163.com>
2017-05-06 02:27:42 +0800

    first commit

---
+++ ReptileNetwork/Main.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace ReptileNetwork
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUrl = new System.Windows.Forms.Label();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnDowLoad = new System.Windows.Forms.Button();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblUrl
+            // 
+            this.lblUrl.AutoSize = true;
+            this.lblUrl.Location = new System.Drawing.Point(12, 21);
+            this.lblUrl.Name = "lblUrl";
+            this.lblUrl.Size = new System.Drawing.Size(41, 12);
+            this.lblUrl.TabIndex = 0;
+            this.lblUrl.Text = "地址：";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(59, 18);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(294, 21);
+            this.txtUrl.TabIndex = 1;
+            // 
+            // btnDowLoad
+            // 
+            this.btnDowLoad.Location = new System.Drawing.Point(379, 21);
+            this.btnDowLoad.Name = "btnDowLoad";
+            this.btnDowLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnDowLoad.TabIndex = 2;
+            this.btnDowLoad.Text = "下载";
+            this.btnDowLoad.UseVisualStyleBackColor = true;
+            this.btnDowLoad.Click += new System.EventHandler(this.btnDowLoad_Click);
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(59, 69);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtContent.Size = new System.Drawing.Size(642, 370);
+            this.txtContent.TabIndex = 3;
+            this.txtContent.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtContent_KeyDown);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(713, 464);
+            this.Controls.Add(this.txtContent);
+            this.Controls.Add(this.btnDowLoad);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.lblUrl);
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUrl;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnDowLoad;
+        private System.Windows.Forms.TextBox txtContent;
+    }
+}
+
---
+++ ReptileNetwork/Main.cs
@@ -0,0 +1,61 @@
+﻿using DotNet.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReptileNetwork
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDowLoad_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(txtUrl.Text))
+            {
+                MessageBox.Show("请输入URL");
+                return; 
+            }
+            HttpHelper http = new HttpHelper();
+            HttpItem item = new HttpItem()
+            {
+                URL = txtUrl.Text,//URL     必需项    
+                Method = "get",//URL     可选项 默认为Get   
+                IsToLower = false,//得到的HTML代码是否转成小写     可选项默认转小写   
+                Cookie = "",//字符串Cookie     可选项   
+                Referer = "",//来源URL     可选项   
+                Postdata = "",//Post数据     可选项GET时不需要写   
+                Timeout = 100000,//连接超时时间     可选项默认为100000    
+                ReadWriteTimeout = 30000,//写入Post数据超时时间     可选项默认为30000   
+                UserAgent = "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)",//用户的浏览器类型，版本，操作系统     可选项有默认值   
+                ContentType = "text/html",//返回类型    可选项有默认值   
+                Allowautoredirect = false,//是否根据301跳转     可选项   
+                //CerPath = "d:\123.cer",//证书绝对路径     可选项不需要证书时可以不写这个参数   
+                //Connectionlimit = 1024,//最大连接数     可选项 默认为1024    
+                ProxyIp = "",//代理服务器ID     可选项 不需要代理 时可以不设置这三个参数    
+                //ProxyPwd = "123456",//代理服务器密码     可选项    
+                //ProxyUserName = "administrator",//代理服务器账户名     可选项   
+                ResultType = ResultType.String
+            };
+            HttpResult result = http.GetHtml(item);
+            txtContent.Text = result.Html;
+        }
+
+        private void txtContent_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Modifiers == Keys.Control && e.KeyCode == Keys.A)
+            {
+                ((TextBox)sender).SelectAll();
+            }
+        }
+    }
+}
